081724483bd08e0d76ac0c655614479d00c51ede
haru <princexbundela@gmail.com>
2026-02-02 09:32:19 +0530

    js-project-prtc

--- index.js
+++ index.js
@@ -1,45 +1,19 @@
-let arr = [
-    {
-        nam:"prince",
-        age:18, 
-        height :5.6,
-        weight:58
-    },
-    {
-        nam:"rahul",
-        age:18, 
-        height :5.7,
-        weight:58
-    },
-    {
-        nam:"john",
-        age:18, 
-        height :5.7,
-        weight:58
-    },
-    {
-        nam:"doe",
-        age:18, 
-        height :5.7,
-        weight:58
-    },
-    {
-        nam:"smith",
-        age:18, 
-        height :5.7,
-        weight:58
-    },
-    {
-        nam:"wick",
-        age:18, 
-        height :5.7,
-        weight:58
-    },
-
-]
 let btn = document.querySelector("button");
-let h1 = document.querySelector("h1");
-btn.addEventListener("click",()=>{
-    let random = Math.floor(Math.random()*arr.length);
-    h1.innerText = `Name : ${arr[random].nam} `;
-})
+let main = document.querySelector("main");
+let colors = ["teal", "blue", "aqua", "green", "purple", "pink", "yellow", "orange", "red", "brown"];
+
+btn.addEventListener("click", function() {
+    let randomColor = colors[Math.floor(Math.random() * colors.length)];
+    let div = document.createElement("div");
+    div.style.backgroundColor = randomColor;
+    console.log(randomColor,colors.indexOf(randomColor));
+    div.innerHTML = randomColor;
+    main.style.overflow = "hidden";
+    div.style.height = "7rem";
+    div.style.width = "7rem";
+    div.style.position = "absolute";
+    div.style.top = Math.random() * 100 + "%";
+    div.style.left = Math.random() * 100 + "%";
+    main.appendChild(div);
+
+});
