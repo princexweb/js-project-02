66d2aee8b7e64526497261b2b7e177e89e66991f
haru <princexbundela@gmail.com>
2026-02-04 22:54:46 +0530

    half project done

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
-let hi = document.querySelector('h1');
-let body = document.querySelector('body');
-body.addEventListener('keydown', (hello) => {
-  hi.innerHTML = hello.code;
+let body = document.body
+let box = document.getElementById("box")
+let hel = document.querySelectorAll(".hel")
+let audio;
+body.addEventListener("keydown", (event) => {
+    console.log(event.key);
+    audio = new Audio(`./audio/${event.key}.mp3`);
+    audio.play();
+});
+hel.forEach((element) => {
+    element.childNodes[0].addEventListener("click", () => {
+        audio = new Audio(`./audio/${element.id}.mp3`);
+        audio.play();
+    });
 });
